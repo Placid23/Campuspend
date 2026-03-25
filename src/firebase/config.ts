50535b431eeb64880396828ade6ce86c0387c865
Kingsley Enyinnaya <enyikings3778@gmail.com>
2026-03-25 09:22:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1667237920-b5675",
+  "appId": "1:956344002003:web:1d9ea5cddbec298d17bb26",
+  "apiKey": "AIzaSyDKtByHQG2EYm98eSeySStHo83fL5D1qB4",
+  "authDomain": "studio-1667237920-b5675.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "956344002003"
+};
