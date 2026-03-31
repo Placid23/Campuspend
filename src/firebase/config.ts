2953024d3ab3f50beb7cf9f6ce3ebe7dab5c2fae
Kingsley Enyinnaya <enyikings3778@gmail.com>
2026-03-31 11:42:41 +0000

    i want you to use // Import the functions you need from the SDKs you nee

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,10 @@
 export const firebaseConfig = {
-  "apiKey": "AIzaSyDSyIP2UEuXk387laHvXkzcCO9lSyrCWUU",
-  "authDomain": "campusspend-733ab.firebaseapp.com",
-  "projectId": "campusspend-733ab",
-  "storageBucket": "campusspend-733ab.firebasestorage.app",
-  "messagingSenderId": "525745910718",
-  "appId": "1:525745910718:web:8f699c80f561cfd9acffef",
-  "measurementId": "G-KLEVQW11XM"
+  apiKey: "AIzaSyDSyIP2UEuXk387laHvXkzcCO9lSyrCWUU",
+  authDomain: "campusspend-733ab.firebaseapp.com",
+  databaseURL: "https://campusspend-733ab-default-rtdb.firebaseio.com",
+  projectId: "campusspend-733ab",
+  storageBucket: "campusspend-733ab.firebasestorage.app",
+  messagingSenderId: "525745910718",
+  appId: "1:525745910718:web:8d5e12afe3bff39dacffef",
+  measurementId: "G-E7HSTNP3F2"
 };
